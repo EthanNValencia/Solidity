40fa918dbfb93f5f79a01866573f48d7193e288a
EthanNValencia <enephew2@mail.valenciacollege.edu>
2022-05-20 18:09:15 -0400

    Working with unit testing... still.

--- demos/smartcontract-lottery/tests/unit/test_lottery_unit.py
+++ demos/smartcontract-lottery/tests/unit/test_lottery_unit.py
@@ -1,9 +1,8 @@
-from brownie import Lottery, accounts, network, config, web3
+from brownie import Lottery, accounts, network, config, web3, exceptions
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
 import pytest
-
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 
 # 0.0022727272727273 (this is what the rate should be)
 
@@ -18,4 +17,12 @@ def test_get_entrance_fee():
     # usdEntryFee is 5
     # 2000 / 1 == 5/x == 0.0025
     expected_entrance_fee = Web3.toWei(2.5, "ether")  # Act
-    assert entrance_fee == expected_entrance_fee
+    assert entrance_fee == expected_entrance_fee  # Assert
+
+
+def test_cannot_enter_unless_started():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
